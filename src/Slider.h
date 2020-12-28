73ccf44d0386d7bcf443dc25dfac1b17e4b7f325
destroyedlolo <l_faillie@yahoo.com>
2020-12-28 18:25:00 +0100

    Slider's value can be displayed inside it's knob

--- src/Slider.h
+++ src/Slider.h
@@ -6,22 +6,32 @@
 #define SLDR_H
 
 class Slider : public GfxObject {
+	bool dispval;	// Display value on the knob
+
 public:
 	/* Slider constructor
 	 * -> Container *parent : parent object (default : NULL)
 	 * -> Container *cloned : copy from this object (default : NULL)
 	 * -> int16_t min,max : min and maximum value (default 0, 100)
+	 * -> displayval : true, display slider value above
 	 *
 	 *  Notez-bien : the parent style is not copied, as container's one
 	 *  are more annoying than useful.
 	 */
-	Slider( Container *parent=NULL, Container *cloned=NULL, int16_t min=0, int16_t max=100 ){
+	Slider( Container *parent=NULL, Container *cloned=NULL, int16_t min=0, int16_t max=100, bool displayval = false ){
 		this->_obj = lv_slider_create( 
 			parent ? parent->getMyself() : NULL, 
 			cloned ? cloned->getMyself() : NULL
 		);
 
 		lv_slider_set_range( this->_obj, min, max );
+
+		if( (this->dispval = displayval) ){
+		    lv_obj_set_style_local_value_font(this->_obj, LV_SLIDER_PART_KNOB, LV_STATE_DEFAULT, lv_theme_get_font_small());
+		    lv_obj_set_style_local_value_opa(this->_obj, LV_SLIDER_PART_KNOB, LV_STATE_DEFAULT, LV_OPA_60);
+		    lv_obj_set_style_local_value_opa(this->_obj, LV_SLIDER_PART_KNOB, LV_STATE_FOCUSED, LV_OPA_COVER);
+		    lv_obj_set_style_local_transition_time(this->_obj, LV_SLIDER_PART_KNOB, LV_STATE_DEFAULT, 300);
+		}
 	}
 
 	/* Set minimum and the maximum values of the slider
@@ -58,6 +68,31 @@ public:
 	 */
 	void setType( lv_slider_type_t type ){
 		lv_slider_set_type( this->_obj, type );
+	}
+
+	/* Update knob text
+	 *	-> char * : buffer string to hold the result
+	 *	-> const uint8_t size : size of the temporary buffer
+	 *	-> const char *format : printf's style format to display (the value is an integer)
+	 */
+	void refreshKnob( char *t, const uint8_t size, const char *format ){
+		if(this->dispval){
+			snprintf(t, size, format, lv_slider_get_value(this->_obj));
+			lv_obj_set_style_local_value_str( this->_obj, LV_SLIDER_PART_KNOB, LV_STATE_DEFAULT, t);
+		}
+	}
+
+	/* Update knob text
+	 * static version to be used from a callback
+	 * (up to user code to know if it has to be called or not).
+	 *	-> lv_obj_t *obj : slider object
+	 *	-> char * : buffer string to hold the result
+	 *	-> const uint8_t size : size of the temporary buffer
+	 *	-> const char *format : printf's style format to display (the value is an integer)
+	 */
+	static void refreshKnob(lv_obj_t *obj, char *t, const uint8_t size, const char *format ){
+		snprintf(t, size, format, lv_slider_get_value(obj));
+		lv_obj_set_style_local_value_str( obj, LV_SLIDER_PART_KNOB, LV_STATE_DEFAULT, t);
 	}
 };
 
