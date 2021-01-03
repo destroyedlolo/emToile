a9542f714967e9f1603d747b1cdd054942c14e50
destroyedlolo <l_faillie@yahoo.com>
2021-01-03 19:17:15 +0100

    TabView is now working

--- src/TabView.h
+++ src/TabView.h
@@ -20,6 +20,28 @@ public:
 	TabView( lv_obj_t *parent=NULL, const lv_obj_t *cloned=NULL ){
 		this->_obj = lv_tabview_create( parent, cloned );
 	}
+
+	/* Add a tab
+	 * -> const char *name : Text on the tab button
+	 * <- lv_obj_t * : newly created lvgl page for this object
+	 *
+	 * Dev note : at first, I was thinking to create directly a page Object
+	 * here, but it's not smart as it will void all the power of the OOP way
+	 * to create page with sub object in the constructor.
+	 * So only an Lvgl object pointer is returned and then, a Page needs to 
+	 * be created using Page( lv_obj_t *, bool ) constructor.
+	 */
+	 lv_obj_t *AddTab( const char *name ){
+	 	return lv_tabview_add_tab( this->getMyself(), name );
+	 }
+
+	/* Flash if an edge is reached
+	 * -> bool : true flash, false don't flash
+	 */
+	void setEdgeFlash( bool en ){
+		lv_page_set_edge_flash( this->getMyself(), en );
+	}
+
 };
 
 #endif
