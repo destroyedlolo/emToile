16713ef4970f37ec3a95a541d28f6f6bb4fc9df4
destroyedlolo <l_faillie@yahoo.com>
2021-05-13 01:41:54 +0200

    Example where Image displays a symbole

---
+++ examples/Images.cpp
@@ -0,0 +1,30 @@
+/********************************************************************
+* Image with a symbole test
+*
+* Create an image displaying a symbole
+*********************************************************************/
+
+#include <Page.h>
+#include <Image.h>
+
+	/*****
+	 * objects
+	 *****/
+
+Page *pg;
+Image *img;
+
+LV_IMG_DECLARE(chambre_32px);
+
+
+	/**** 
+	 * Build the GUI
+	 *****/
+void start_gui( void ){
+	pg = new Page( lv_scr_act() );
+	pg->setSize( 150, 200 );
+	pg->Align( LV_ALIGN_CENTER );
+
+	img = new Image( pg->getMyself() );
+	img->Set( LV_SYMBOL_BATTERY_FULL );
+}
